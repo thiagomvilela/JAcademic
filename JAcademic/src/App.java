d66d7fd4b3ba7c695204ed46b57c7091aac9b108
Vinicius lima <viniciuslima.vf4@hotmail.com>
2022-01-06 21:28:11 -0300

    Reset Database

--- JAcademic/src/App.java
+++ JAcademic/src/App.java
@@ -5,7 +5,7 @@ import models.dao.StudentDAO;
 public class App {
     public static void main(String[] args) throws Exception 
     {
-
+        AcademicDAO.getInstance().DeleteDatabase();
         AcademicDAO.getInstance().initializeDatabase();
         
         mostrarAlunosENotas();
